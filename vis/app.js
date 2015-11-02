31a811ef72fab928a58ab8173b69d880b6bdc513
ian seyer <ian.seyer@gmail.com>
2015-11-01 20:43:41 -0600

    fixing favicon

--- vis/app.js
+++ vis/app.js
@@ -2,7 +2,6 @@
 
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -21,7 +20,6 @@ app.locals.ENV_DEVELOPMENT = env == 'development';
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
