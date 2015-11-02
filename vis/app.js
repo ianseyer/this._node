6ad0002123ccbbd1d51a6663714680e132e5d6c7
ian seyer <ian.seyer@gmail.com>
2015-11-01 21:18:07 -0600

    favicon

--- vis/app.js
+++ vis/app.js
@@ -28,9 +28,9 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public/favicon.ico')));
 app.use('/', routes);
 app.use('/users', users);
+app.use(favicon(__dirname + 'tacos'));
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
